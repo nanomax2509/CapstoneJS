d19c8ab5b7351be3c1abbf1f1801bb3465a92a30
QuocAnh <nguyenquocanh2509@gmail.com>
2023-06-20 15:43:46 +0700

    update html login,signup, api products

---
+++ controller/DSSP.js
@@ -0,0 +1,28 @@
+function products(name,alias,price,description,size,shortDescription,quantity,deleted,categories,feature,image){
+    this.name = name;
+    this.alias = alias;
+    this.price = price;
+    this.description = description;
+    this.size = size;
+    this.shortDescription = shortDescription;
+    this.quantity = quantity;
+    this.deleted = deleted;
+    this.categories = categories;
+    this.feature = feature;
+    this.image = image;
+}
+/*
+"id": 1,
+      "name": "Adidas Prophere",
+      "alias": "adidas-prophere",
+      "price": 350,
+      "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+      "size": "[36,37,38,39,40,41,42]",
+      "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+      "quantity": 995,
+      "deleted": false,
+      "categories": "[{\"id\":\"ADIDAS\",\"category\":\"ADIDAS\"},{\"id\":\"MEN\",\"category\":\"MEN\"},{\"id\":\"WOMEN\",\"category\":\"WOMEN\"}]",
+      "relatedProducts": "[2,3,5]",
+      "feature": true,
+      "image": "https://shop.cyberlearn.vn/images/adidas-prophere.png"
+*/
--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,84 @@
+function layDanhSachSP() {
+  axios({
+    method: 'get',
+    url: 'https://shop.cyberlearn.vn/api/Product',
+  }).then(function (result) {
+    console.log(result);
+    hienThiSP(result.data);
+  }).catch(function (error) {
+    //thất bại
+    console.log(error);
 
-export const getAllProductApi = async (dispatch) => {
-    try {
-      const fetch = await http.get("https://shop.cyberlearn.vn/api/Product");
-      dispatch(updateProductReducer(fetch.data.content));
-      dispatch(updateProductRandomCarousel(suffleArray(fetch.data.content, 5)));
-    } catch (error) {
+  });
+};
+//Gọi khi load web để có sẵn dữ liệu cho table
+layDanhSachSP();
+
+  function sapXepLoai(category){
+    axios({
+      method: 'get',
+      url: `https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=${category}`,
+    }).then(function (result) {
+      console.log(result);
+      hienThiSP(result.data);
+    }).catch(function (error) {
+      //thất bại
       console.log(error);
-    }
+
+    });
   };
+  const categoryList = document.getElementById("data-category");
+  categoryList.addEventListener('click', function(event){
+    const categoryId = event.target.dataset.categoryId;
+    sapXepLoai(categoryId);
+  });
+
+
+function hienThiSP(products) {
+  let productHTML = '';
+  console.log(products);
+  products.content.map(function (product, index) {
+    productHTML += `
+    <div class="col-3 mt-3">
+    <div class="products__item">
+      <div class="products__imgJS">
+      <img src="${product.image}" alt="">
+        <div class="products__hiddenJS">
+          <div class="products__heartJS">
+            <a href="#"><i class="fa-solid fa-heart"></i></a>
+          </div>
+          <div class="products__signalJS">
+            <a href="#"><i class="fa-solid fa-signal"></i></a>
+          </div>
+          <div class="products__viewJS">
+            <a href="#"><i class="fa-solid fa-magnifying-glass-plus"></i> quick view</a>
+          </div>
+        </div>
+      </div>
+      <div class="product__titleJS">
+        <h4>${product.name}</h4>
+        <div class="products__ratingJS">`;
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= product.rating) {
+        productHTML += '<i class="fa-solid fa-star"></i>';
+      } else {
+        productHTML += '<i class="fa-regular fa-star"></i>';
+      }
+    }
+
+    productHTML += `
+        </div>
+        <div class="products__priceJS">
+          <span>$ ${product.price}</span>
+        </div>
+        <div class="product__buttonJS">
+          <a href="#"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+        </div>
+      </div>
+    </div>
+    </div>`;
+  });
+  document.querySelector("#productSP").innerHTML = productHTML;
+
+};
